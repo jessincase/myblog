20ceb72afbf1da293bd7eb2c6ac8564fc69bbeb5
jessincase <au_jessica.huang@hotmail.com>
2017-09-12 01:28:06 +1000

    Created blog app

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n5*5hyjuvyak)tb5+^)33ain28&$ci400mceho%q*x#_#k)l)s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Sydney'
 
 USE_I18N = True
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES = os.path.join(BASE_DIR, 'static')
